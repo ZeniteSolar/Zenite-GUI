bce96de90f60bdc9d9ecb7f347c952d6b55af1eb
ayresgit <ayres808@gmail.com>
2022-07-18 11:14:20 -0300

    Declared basic methods and atributes

--- QtZeniteInterface/gps.h
+++ QtZeniteInterface/gps.h
@@ -1,11 +1,25 @@
 #ifndef GPS_H
 #define GPS_H
 
+#include "main.h"
+
+#include <time.h>
 
 class GPS
 {
 public:
     GPS();
+    float GetLatitude();
+    float GetLongitude();
+
+    std::vector<time_t*> GetTimeToday();
+    struct GetTimeCalendar();
+    ReadTime();
+    ReadPosition();
+
+private:
+    struct tm _time_struct;
+    float latitude, longitude;
 };
 
 #endif // GPS_H
