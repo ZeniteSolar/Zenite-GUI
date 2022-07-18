bc04b8b22f01e595b9e993f037575f99cfc627b4
ayresgit <ayres808@gmail.com>
2022-07-18 08:19:27 -0300

    Added Compass Class with basic methods and atributes

--- QtZeniteInterface/compass.cpp
+++ QtZeniteInterface/compass.cpp
@@ -2,5 +2,27 @@
 
 Compass::Compass()
 {
+    this->_x_mag = 0;
+    this->_x_mag = 0;
+    this->_x_mag = 0;
+    this->heading = 0;
+}
 
+void Compass::SetHeading(heading_angle)
+{
+    this->_heading = heading_angle;
+}
+void Compass::SetRaw(float x, float y, float z)
+{
+    this->_x_mag = x;
+    this->_y_mag = y;
+    this->_z_mag = z;
+}
+float Compass::GetHeading()
+{
+    return _heading;
+}
+vector<float*> Compass::GetRaw()
+{
+    return _mag_pointers;
 }
--- QtZeniteInterface/compass.h
+++ QtZeniteInterface/compass.h
@@ -1,11 +1,24 @@
 #ifndef COMPASS_H
 #define COMPASS_H
 
+#include "main.h"
+#include <vector>
 
 class Compass
 {
 public:
     Compass();
+
+    void SetHeading(float heading_angle);
+    void SetRaw(float x, float y, float z);
+    float GetHeading();
+    vector<float*> GetRaw();
+
+private:
+    const vector<float*> _mag_pointers {&_x_mag, &_y_mag, &_z_mag}; 
+    float _x_mag, _y_mag, _z_mag;
+    float _heading;
+
 };
 
 #endif // COMPASS_H
