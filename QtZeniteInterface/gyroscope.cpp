c1c26c1d4b1806352bb1fce88f6bf140cbc91581
Gabriel Ayres <ayres808@gmail.com>
2022-07-25 11:21:40 -0300

    Class uses accelerations and gyro struct

--- QtZeniteInterface/gyroscope.cpp
+++ QtZeniteInterface/gyroscope.cpp
@@ -2,5 +2,27 @@
 
 Gyroscope::Gyroscope()
 {
+    SetGyroRaw(0, 0, 0, 0, 0, 0);
+}
+
+void Gyroscope::SetGyroRaw(float ax, float ay, float az, float gx, float gy, float gz)
+{
+    _my_gyro._accelX = ax;
+    _my_gyro._accelY = ay;
+    _my_gyro._accelZ = az;
+
+    _my_gyro._gyroX = gx;
+    _my_gyro._gyroY = gy;
+    _my_gyro._gyroZ = gz;
+}
+
+Gyroscope::sensor_gyro Gyroscope::GetGyro()
+{
+    return _my_gyro;
+}
+
+//TODO:
+void Gyroscope::CalculateYaw(sensor_gyro gyro_structure)
+{
 
 }
--- QtZeniteInterface/gyroscope.h
+++ QtZeniteInterface/gyroscope.h
@@ -6,6 +6,22 @@ class Gyroscope
 {
 public:
     Gyroscope();
+
+    struct sensor_gyro { float _accelX, _accelY, _accelZ, 
+                            _gyroX, _gyroY, _gyroZ, 
+                            _yaw, _pitch, _row; };
+
+    sensor_gyro GetGyro();
+
+private:
+    // Planned friend class CAN
+    friend class Acceleration;
+    sensor_gyro _my_gyro;
+
+    void SetGyroRaw(float ax, float ay, float az, float gx, float gy, float gz);
+    
+    //TODO:
+    void CalculateYaw(sensor_gyro gyro_structure);
 };
 
 #endif // GYROSCOPE_H
